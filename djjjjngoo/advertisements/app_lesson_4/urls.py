3295c261f1d87998969319b651beba24df98b700
vv1 <vvv@dkk>
2023-07-16 13:56:05 +0300

    дз

--- djjjjngoo/advertisements/app_lesson_4/urls.py
+++ djjjjngoo/advertisements/app_lesson_4/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
-urlpatterns=[
-    path('', index)
+urlpatterns = [
+    path('lll', index, name='main-page'),
+    path('top-sel', top_sellers, name='top-sellers'),
 ]
